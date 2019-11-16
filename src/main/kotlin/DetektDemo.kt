8dd2a810fb645f2ec30d1d05816e4e6b1f1ace94
tokgozmusa <tokgozmusa@gmail.com>
2019-11-16 13:07:24 +0100

    test

--- src/main/kotlin/DetektDemo.kt
+++ src/main/kotlin/DetektDemo.kt
@@ -1,9 +1,1 @@
-class DetektDemo {
-    companion object {
-        private const val CONSTANT_NUMBER = 5
-    }
-    fun demo1() {
-        val x = CONSTANT_NUMBER
-        println(x)
-    }
-}
+class DetektDemo {}
