b1db8249b3b7765813bc2400c54d7c55f4c41ab9
tokgozmusa <tokgozmusa@gmail.com>
2019-11-16 13:26:18 +0100

    test

--- src/main/kotlin/DetektDemo.kt
+++ src/main/kotlin/DetektDemo.kt
@@ -1,1 +1,51 @@
-class DetektDemo {}
+class DetektDemo {
+    private   val unusedVariable :   String = "unused"
+
+    fun isHTTPSuccess(statusCode: Int): Boolean = statusCode == 200
+
+    fun veryLongFunction(){
+        println("Hello")
+        println("Hello")
+        println("Hello")
+        println("Hello");
+        println("Hello")
+        println("Hello")
+        println("Hello")
+        println("Hello");
+        println("Hello")
+        println("Hello")
+    }
+
+    fun fixMyStyle(x:Int)   : Int {
+        when(x){
+            1 ->{
+                println(x)
+                return x
+            }
+            2 ->{
+                println(x)
+                return x
+            }
+            else -> {throw Exception()}
+        }
+    }
+
+    fun notCompletedFunction() {
+        // TODO: fix this function
+        println("not completed")
+    }
+
+    // This is a tmp hack solution
+    fun hackyFunction(){
+        println("hack solution")
+    }
+
+    fun unsafeFunction(){
+        var user: User? = null
+        println(user!!.name)
+    }
+}
+
+class User {
+    val name = "User"
+}
