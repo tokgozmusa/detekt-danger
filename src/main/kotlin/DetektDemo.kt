5b811d8ae8dfe6dcb9dbed32ebc1064eebbe0970
tokgozmusa <tokgozmusa@gmail.com>
2019-11-16 13:00:10 +0100

    test

--- src/main/kotlin/DetektDemo.kt
+++ src/main/kotlin/DetektDemo.kt
@@ -1,5 +1,9 @@
 class DetektDemo {
+    companion object {
+        private const val CONSTANT_NUMBER = 5
+    }
     fun demo1() {
-        val x = 5
+        val x = CONSTANT_NUMBER
+        println(x)
     }
 }
--- src/main/kotlin/PSIDemo.kt
+++ src/main/kotlin/PSIDemo.kt
@@ -1,10 +1,6 @@
 class PSIDemo {
     fun sayHello() {
-        // TODO: test-123
         val str = "Hello"
         println(str)
-        println("abc")
-        println("xyz")
-        println("xyz")
     }
 }
